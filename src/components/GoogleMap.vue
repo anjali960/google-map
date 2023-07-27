01e6be7a15291943ff863988aad3382e52ad5f18
anjali960 <anjalibaghel960@gmail.com>
2023-07-27 14:08:31 +0530

    adding gmap

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    heyyyyyyyyyyyy
+    <gmap-map
+      :center="center"
+      :zoom="10"
+      style="width: 100%; height: 600px"
+    ></gmap-map>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GoogleMap",
+  data() {
+    return {
+      center: { lat: 45.5, lng: -46.64 },
+    };
+  },
+};
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import * as VueGoogleMaps from "vue2-google-maps";
+import VueGeoLocation from "vue-browser-geolocation";
 
-Vue.config.productionTip = false
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyA4KqG0k2_1s8cjnNn4mcLOerfaT_Aahx8",
+    libraries: "places",
+  },
+});
+Vue.use(VueGeoLocation);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
